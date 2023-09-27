046d5d0293d5c575719ac00776cc344da3d286d3
mitory <lairziro@gmail.com>
2023-09-27 20:12:33 +0300

    update

--- src/components/LK.vue
+++ src/components/LK.vue
@@ -1,10 +1,10 @@
 <template>
     <div class='container'>
-        <div class="row gx-sm-5 justify-content-end flex-sm-row flex-column-reverse">
+        <div class="row gx-3 gx-md-5 justify-content-end flex-sm-row flex-column-reverse">
             <div class="col-sm-8">
-                <router-view class="border p-1 p-lg-5"></router-view>
+                <router-view class="border p-3 p-md-5"></router-view>
             </div>
-            <div class="col-sm-3 d-sm-block mb-sm-0 mb-2">
+            <div class="col-sm-4 col-md-3 d-sm-block mb-sm-0 mb-2">
                 <LKNavBar class="align-self-center"></LKNavBar>
             </div>
         </div>
