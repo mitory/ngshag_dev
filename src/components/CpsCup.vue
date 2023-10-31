a2838f364e57ed3d6042731c833bc9d7fd91a1bb
mitory <lairziro@gmail.com>
2023-11-01 00:23:47 +0300

    update

--- src/components/CpsCup.vue
+++ src/components/CpsCup.vue
@@ -4,24 +4,24 @@
         <p class="fs-6 text-primary"><em>3-5 ноября</em></p>
         <p><em>50 000 рублей*</em> для победителя в каждой из номинаций найдут своего чемпиона.
             Дополнительно победитель получит возможность трудоустройства по выбранному направлению.</p>
-        <p><strong>Оргкомитетом принято решение о свободном доступе к конкурсу по первым пяти номинациям.</strong> Это
-            значит, что каждый может выбрать номинацию и решить конкурсные задачи на очном этапе.</p>
+        <p><strong>Оргкомитетом принято решение о свободном доступе к конкурсу по первым пяти номинациям.</strong> </p>
 
-        <p class="text-primary">
+        <div>
+            <h6 v-if="nominations.length">Вы записаны на следующие номинации:</h6>
+            <ul class="list-group list-group-flush mb-2">
+                <li class="list-group-item link-nomination" v-for="nomination, index in nominations" :key="index">
+                    {{ nomination }}
+                </li>
+            </ul>
+        </div>
+
+        <p v-if="false" class="text-primary">
             Формируется таблица соревнования. Пожалуйста, обозначь свое намерение об участии <em>до 31 октября</em>. Ждём
             твоего отклика, чтобы сформировать удобное расписание.
         </p>
 
-        <!-- <div v-else>
-            <h6>Вы записаны на следующие номинации:</h6>
-            <ul class="list-group list-group-flush mb-2">
-                <li class="list-group-item link-nomination" v-for="res, index in result" :key="index">
-                    {{ res.nomination.name }}
-                </li>
-            </ul>
-        </div> -->
-
-        <div v-if="isNonActive != undefined && events != []" class="d-flex flex-column">
+        <!-- isNonActive != undefined && events != [] -->
+        <div v-if="false" class="d-flex flex-column">
             <div class="mb-3 mx-1" v-for="event, index in events" :key="event.id">
                 <div class="d-flex justify-content-between">
                     <h6 class="mb-2">{{ event.title }}</h6>
@@ -36,6 +36,7 @@
                 <hr>
             </div>
         </div>
+        <hr>
         <p class="text-primary">*По условиям участия, студенты, ставшие победителями Кубка НИИ «Центрпрограммсистем» получат
             дополнительную стипендию, даже если учатся платно.</p>
 
@@ -53,7 +54,8 @@ export default {
             events: [],
 
             isNonActive: [],
-            isEmailNonAuth: false
+            isEmailNonAuth: false,
+            nominations: []
         }
     },
     components: {
@@ -70,6 +72,9 @@ export default {
                 try {
                     const response = await userService.getUserEvent(event.id);
                     this.isNonActive.push(!response.data);
+                    if (response.data) {
+                        this.nominations.push(event.title)
+                    }
                 } catch (error) {
                     if (error.status && [400, 403].includes(error.status)) {
                         this.$store.dispatch('alert/sendMessage', { message: error.error, type: 'Danger' });
